473dcdb1cbb2fe62bacab69e7da845001df7293a
gspeager <gbits.dev@gmail.com>
2011-08-13 20:28:52 -0600

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MediaManager::Application.config.secret_token = '84e90b168febf2f6f087b0b95ef4dbaa39273b301c69841d4fde22cbb011cea842df35b9f43bf1cbbfcf9613b2028f9e0bd12c091daa568d38c1bd2ad5b7230d'
