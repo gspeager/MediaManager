a64c335a663f3784c817d1e031d6717f9be3ec7c
gspeager <gbits.dev@gmail.com>
2011-09-01 23:30:23 -0600

    Closes #17  * Updates CSS  * Adds Ids to listing divs  * Adds jQuery to handle song list accordion functionality

---
+++ app/assets/javascripts/songListView.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+ 
+	$('.editLink').click(function(){
+		var editLink = $(this);
+		$('#songEditSection_' + this.id.split('_')[1]).slideToggle('slow');
+		if(editLink.text() == 'edit') editLink.text('close');
+		else editLink.text('edit')
+	});
+ 
+});
