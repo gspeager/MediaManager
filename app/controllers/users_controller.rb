c05ad4ce1bc9e837d1ab4140295ba477b2cd77e3
gspeager <gbits.dev@gmail.com>
2011-09-19 20:53:59 -0600

    [REQUIRES DB MIGRATION] Modifies the authentication
     * Adds token field to user to store as cookie
     * Adds checkbox to store cookie for a long time to login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,16 @@ class UsersController < ApplicationController
   force_ssl
   
   def new
-  	@user = User.new
+    @user = User.new
   end
 
   def create
-  	@user = User.new(params[:user])
-  	if @user.save
-      session[:user_id] = @user.id
-  		redirect_to root_url, :notice => "Signed up!"
-  	else
-  		render "new"
-  	end
+    @user = User.new(params[:user])
+    if @user.save
+      cookies[:auth_token] = @user.auth_token
+      redirect_to root_url, :notice => "Signed up!"
+    else
+      render "new"
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ActiveRecord::Base
   validates_presence_of :password, :on => :create
   validates_presence_of :email
   validates_uniqueness_of :email
+
+  before_create { generate_token(:auth_token) }
+
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
+
 end
